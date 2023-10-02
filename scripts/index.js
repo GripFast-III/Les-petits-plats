d3fc20bcd8be89861f973aeb399015ff7860f3c4
GripFast-III <jeandbdp@gmail.com>
2023-10-02 06:28:09 +0200

    Ajout des listes avec toggle pour les filtres

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,47 @@ async function getRecipes() {
     const response = await fetch("./../recipes.json");
     if (response.ok) {
       const recipesData = await response.json();
-      return recipesData.recipes;
+      const recipes = recipesData.recipes; // Appel la fonction qui récupère les ingrédients, appareils et ustensiles
+      const { uniqueIngredients, uniqueAppliances, uniqueUstensils } =
+        getUniqueStuff(recipes);
+      console.log("Ingrédients uniques:", Array.from(uniqueIngredients));
+      console.log("Appareils uniques:", Array.from(uniqueAppliances));
+      console.log("Ustensils uniques:", Array.from(uniqueUstensils));
+      return recipes;
     } else {
       throw new Error("Erreur de la récupération de la donnéee");
     }
   } catch (error) {
     return error;
   }
+}
+
+// Fonction qui récupère les ingrédients appareils et ustensils
+function getUniqueStuff(recipes) {
+  const uniqueIngredients = new Set();
+  const uniqueAppliances = new Set();
+  const uniqueUstensils = new Set();
+
+  // Récupère les ingrédients
+  recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((ingredient) => {
+      uniqueIngredients.add(ingredient.ingredient.toLowerCase()); // Les ingrédients et les ustensils sont stockés sous forme de tableau
+    });
+
+    // Récupère les appareils
+    uniqueAppliances.add(recipe.appliance.toLowerCase()); // Les appareils sont stockés dans chaque recette comme une chaine de caractères puisqu'il n'y a qu'un seul appareil par recette
+
+    // Récupère les ustensiles
+    recipe.ustensils.forEach((ustensil) => {
+      uniqueUstensils.add(ustensil.toLowerCase());
+    });
+  });
+
+  return {
+    uniqueIngredients,
+    uniqueAppliances,
+    uniqueUstensils,
+  };
 }
 
 // Génère les éléments HTML d'une recette
@@ -44,6 +78,7 @@ function createRecipeCard(recipe) {
 
   let templateCard = `
   <div class="card-content">
+    <div class="recipe-time">${time} min</div>
     <div class="img-recipe">
       <img
         src="images/recipes/${image}"
@@ -96,6 +131,49 @@ async function displayRecipes() {
 
 // Appel la fonction pour afficher les recettes
 displayRecipes();
+
+// Sélectionne tous les en-têtes de filtres et leurs listes correspondantes
+const filterHeaders = document.querySelectorAll(".filter-header");
+const filterLists = document.querySelectorAll(".options-list");
+
+// Fonction pour gérer l'ouverture/fermeture de la liste et la rotation du chevron
+function toggleList(header, list) {
+  const chevronIcon = header.querySelector(".fa-chevron-down");
+  if (list.classList.contains("hidden")) {
+    list.classList.remove("hidden");
+    chevronIcon.classList.add("rotate-180");
+  } else {
+    list.classList.add("hidden");
+    chevronIcon.classList.remove("rotate-180");
+  }
+}
+
+// Ajoute un gestionnaire d'événement de clic à chaque en-tête de filtre
+filterHeaders.forEach((header, index) => {
+  header.addEventListener("click", () => {
+    const filterList = filterLists[index];
+    toggleList(header, filterList);
+  });
+});
+
+/*
+// Ajoute un gestionnaire d'événement de clic à chaque en-tête de filtre
+filterHeaders.forEach((header, index) => {
+  header.addEventListener("click", () => {
+    // Affiche la liste correspondante
+    const filterList = filterLists[index];
+
+    // Bascule la classe "hidden" pour montrer ou cacher la liste
+    filterList.classList.toggle("hidden");
+
+    // Sélectionne l'icône du chevron
+    const chevronIcon = header.querySelector(".fa-chevron-down");
+
+    // Bascule la classe "rotate-180" pour faire pivoter l'icône
+    chevronIcon.classList.toggle("rotate-180");
+  });
+});
+*/
 
 getRecipes()
   .then((data) => {
